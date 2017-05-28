9273157add8edbc1a557358ea1ab7d243493bb6c
rav3n <blgrav3n@gmail.com>
2017-05-28 11:53:26 +0900

    init rep

---
+++ app/src/main/java/mobisapps/passbook/StartActivity.kt
@@ -0,0 +1,12 @@
+package mobisapps.passbook
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
